4c582f17f57e52f662e1fafacd43ca23ced9f905
CleanAZCoding <founder@cleanazcoding.com>
2026-07-28 19:51:26 -0400

    updates

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,6 +1,10 @@
-import { ReactNode } from "react";
+import { ReactNode, Suspense } from "react";
 import { HapioProvider } from "@/context/hapio-contex";
 
 export default function HapioLayout({ children }: { children: ReactNode }) {
-  return <HapioProvider>{children}</HapioProvider>;
+  return (
+    <Suspense fallback={null}>
+      <HapioProvider>{children}</HapioProvider>
+    </Suspense>
+  );
 }
